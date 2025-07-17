c35c3992938b8f7bde3fbde759b69cb781bb5e34
Yunus Emre Gültepe <yunus.gultepe@ogr.sakarya.edu.tr>
2025-07-18 00:05:45 +0300

    fix(ci): Correct path to kustomize overlay

--- flink_app/fraud_detector.py
+++ flink_app/fraud_detector.py
@@ -186,3 +186,4 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     # Run the main job definition function.
     main()
+    
